d3e7e6be1269d96577ad2af8a7caff1a0d56ab67
Muhammad Ali Zulfiqar <50575750+MuhammAliZulfiqar@users.noreply.github.com>
2021-09-27 23:54:09 +0500

    Name Entry

--- Ali_Work_On_GitHub/Program.cs
+++ Ali_Work_On_GitHub/Program.cs
@@ -6,11 +6,18 @@ namespace Ali_Work_On_GitHub
     {
         static void Main(string[] args)
         {
+            //Age
             Console.WriteLine("Hello World!");
             Console.WriteLine("Please Enter Your Age");
             int Age;
             Age= Convert.ToInt32(Console.Readline();
             Console.WriteLine("Your age is {0}",Age);
+            
+            //Name
+            Console.WriteLine("Enter Your Name")
+            string Name;
+            Name=Convert.toString(Console.Readline());
+            Console.WriteLine("Welcome {0}",Name);
          }
     }
 }
