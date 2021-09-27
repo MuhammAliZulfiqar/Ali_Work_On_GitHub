471e148f9dfcc2437c6efbd6784531d6f4b3cab3
Muhammad Ali Zulfiqar <50575750+MuhammAliZulfiqar@users.noreply.github.com>
2021-09-27 22:27:02 +0500

    yes its fine

--- Ali_Work_On_GitHub/Program.cs
+++ Ali_Work_On_GitHub/Program.cs
@@ -8,6 +8,9 @@ namespace Ali_Work_On_GitHub
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Please Enter Your Age");
-                    }
+            int Age;
+            Age= Convert.ToInt32(Console.Readline();
+            Console.WriteLine("Your age is {0}",Age);
+         }
     }
 }
