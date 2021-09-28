ea8a31390808cab8624fcba0317c0ce8bbf53354
Muhammad Ali Zulfiqar <50575750+MuhammAliZulfiqar@users.noreply.github.com>
2021-09-28 10:03:37 +0500

    deleate some data

--- Ali_Work_On_GitHub/Program.cs
+++ Ali_Work_On_GitHub/Program.cs
@@ -9,15 +9,7 @@ namespace Ali_Work_On_GitHub
             //Age
             Console.WriteLine("Hello World!");
             Console.WriteLine("Please Enter Your Age");
-            int Age;
-            Age= Convert.ToInt32(Console.Readline();
-            Console.WriteLine("Your age is {0}",Age);
-            
-            //Name
-            Console.WriteLine("Enter Your Name")
-            string Name;
-            Name=Convert.toString(Console.Readline());
-            Console.WriteLine("Welcome {0}",Name);
+            Console.WriteLine("New line");
          }
     }
 }
