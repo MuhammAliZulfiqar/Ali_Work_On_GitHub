81d8cf4ba7e1b5c2349dd6e479f80726700427ae
Muhammad Ali Zulfiqar <50575750+MuhammAliZulfiqar@users.noreply.github.com>
2021-09-28 10:25:24 +0500

    ALi Work

--- Ali_Work_On_GitHub/Program.cs
+++ Ali_Work_On_GitHub/Program.cs
@@ -6,7 +6,6 @@ namespace Ali_Work_On_GitHub
     {
         static void Main(string[] args)
         {
-            //Age
             Console.WriteLine("Hello World!");
             Console.WriteLine("Please Enter Your Age");
             Console.WriteLine("New line");
